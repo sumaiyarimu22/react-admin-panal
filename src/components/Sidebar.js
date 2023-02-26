a8970ee2d4a3d6e47c690b8f272c1223ac2abc16
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-02-26 19:48:44 +0600

    sidebar styleing done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,10 @@
 import logo from "../imgs/logo.png";
-
+import { SidebarData } from "../Data/Data";
+import { UilSignOutAlt } from "@iconscout/react-unicons";
+import { useState } from "react";
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+
   return (
     <div className="sidebar">
       <div className="logo">
@@ -9,7 +13,24 @@ const Sidebar = () => {
           sh<span>o</span>ps
         </span>
       </div>
-      <div className="menu"></div>
+      <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <div
+              className={selected === index ? "menuItem active" : "menuItem"}
+              key={index}
+              onClick={() => setSelected(index)}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
+
+        <div className="menuItem">
+          <UilSignOutAlt />
+        </div>
+      </div>
     </div>
   );
 };
